2a8ac5d503996b8bba974bc06570aa477797582c
offcourseamsterdam <info@offcourseamsterdam.com>
2026-05-05 12:52:19 +0200

    fix(images): allow test.offcourseamsterdam.com as image hostname

--- next.config.ts
+++ next.config.ts
@@ -20,6 +20,10 @@ const nextConfig: NextConfig = {
       },
       {
         protocol: 'https',
+        hostname: '*.offcourseamsterdam.com',
+      },
+      {
+        protocol: 'https',
         hostname: 'offcourseamsterdam.com',
       },
     ],
